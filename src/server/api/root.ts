5412f71023bea979a70c3fb1cdc3139566a17632
RodrigoDutraF88 <rodrigodutraf88@gmail.com>
2026-06-21 17:56:48 -0300

    fix: uncomment createCaller export in root router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,23 +1,20 @@
-import { createTRPCRouter } from "../api/trpc";
+import { createCallerFactory, createTRPCRouter } from "../api/trpc";
 
-// /**
-//  * This is the primary router for your server.
-//  *
-//  * All routers added in /api/routers should be manually added here.
-//  */
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
 export const appRouter = createTRPCRouter({});
 
-// // export type definition of API
+// export type definition of API
 export type AppRouter = typeof appRouter;
 
-// /**
-//  * Create a server-side caller for the tRPC API.
-//  * @example
-//  * const trpc = createCaller(createContext);
-//  * const res = await trpc.post.all();
-//  *       ^? Post[]
-//  */
-// export const createCaller = createCallerFactory(appRouter);
-
-
-
+/**
+ * Create a server-side caller for the tRPC API.
+ * @example
+ * const trpc = createCaller(createContext);
+ * const res = await trpc.post.all();
+ *       ^? Post[]
+ */
+export const createCaller = createCallerFactory(appRouter);
